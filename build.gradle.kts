3c96296884b16e4e7ea27baccfdacb07c76e81b5
lyro7 <lovro.lupis7@gmail.com>
2025-11-17 21:48:08 +0100

    Updated jlink zip file name

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,9 +38,9 @@ tasks.withType<Test> {
 }
 
 jlink {
-    imageZip.set(layout.buildDirectory.file("/distributions/app-${javafx.platform.classifier}.zip"))
+    imageZip.set(layout.buildDirectory.file("/distributions/MoleMadness-${javafx.platform.classifier}.zip"))
     options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
     launcher {
-        name = "app"
+        name = "MoleMadness"
     }
 }
